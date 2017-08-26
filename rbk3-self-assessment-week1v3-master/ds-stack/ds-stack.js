c19b49c3f01082a0f38b4afa22ab5f8f65bafbf2
hibaAkroush <hiba.akr@gmail.com>
2017-08-26 09:48:15 +0300

    changed the stack question

--- rbk3-self-assessment-week1v3-master/ds-stack/ds-stack.js
+++ rbk3-self-assessment-week1v3-master/ds-stack/ds-stack.js
@@ -1,15 +1,21 @@
 var Stack = function() {
-    this._storage = {}
-    this.counter = 0;
-    this.add = function(value){
+  instance={}
+    instance._storage = {}
+    instance.counter = 0;
+    instance.add = function(value){
       counter ++ 
       this._storage[counter]=value
       console.log(this._storage)
     };
-    this.remove = function() {
-      counter --
+    instance.remove = function() {
       delete this._storage[counter]
+            counter --
       console.log(this._storage)
     };
-    return Stack
+    return instance
   };
+
+//   ## Stack
+// * [ ] Implement a stack class.
+//   * You can use an array for storage, and delegate to its methods
+//   * The stack should only have `.add`, `.remove`, and `._storage`, with no other properties or methods (you can test this by calling Object.keys() on your stack instance).
